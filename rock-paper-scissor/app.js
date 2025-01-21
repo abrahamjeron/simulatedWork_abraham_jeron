bc7aca83f89bba394f1c0bd8a6f2ce35ca6fd88a
abrahamjeron <abrahamjeron40@gmail.com>
2025-01-21 13:13:04 +0530

    completed all the ui functionalities

--- rock-paper-scissor/app.js
+++ rock-paper-scissor/app.js
@@ -2,9 +2,28 @@ let rockButton = document.getElementById('input-rock');
 let paperButton = document.getElementById('input-paper');
 let scissorButton = document.getElementById('input-scissor');
 let userInput=''
-let playButton = document.getElementById('play')
-let result = document.getElementById('result')
+let playButton = document.getElementById('play');
+let resetBtn=document.getElementById('reset-btn');
+let result = document.getElementById('result');
 let choosen = document.getElementById('choosen');
+let playerscore = document.getElementById('score');
+let computerscore=document.getElementById('comp-score');
+
+let playerScore=0
+let computerScore=0
+
+resetBtn.addEventListener('click', () => {
+    playerScore = 0;
+    computerScore = 0;
+    playerscore.innerText = playerScore; 
+    computerscore.innerText = computerScore; 
+    result.innerText = ""; 
+    choosen.innerText = ""; 
+});
+
+
+playerscore.innerText=playerScore
+computerscore.innerText=computerScore
 
 rockButton.addEventListener('click',()=>{
     userInput='rock';
@@ -27,19 +46,24 @@ const randomOutput=()=>{
 }
 const computerInput=randomOutput();
  
-const decideWinner=()=>{
-    if (userInput=='rock' && computerInput=='rock' ||
-     userInput=='paper'&& computerInput=='paper' ||
-     userInput=='scissor'&& computerInput=='scissor'){
-        return 'it s a tie! Computer choosed: '+computerInput;
-     }else if (userInput=='rock'&&computerInput=='scissor' || 
-     userInput=='paper'&&computerInput=='rock' || 
-     userInput=='scissor'&& computerInput=='paper'){
-        return 'player wins! Computer choosed: '+computerInput;
-     }else{
-        return 'computer wins! Computer choosed: '+computerInput;
-     }
-}
+const decideWinner = () => {
+    if (userInput == 'rock' && computerInput == 'rock' ||
+        userInput == 'paper' && computerInput == 'paper' ||
+        userInput == 'scissor' && computerInput == 'scissor') {
+        return 'It\'s a tie! Computer chose: ' + computerInput;
+    } else if (userInput == 'rock' && computerInput == 'scissor' ||
+        userInput == 'paper' && computerInput == 'rock' ||
+        userInput == 'scissor' && computerInput == 'paper') {
+        playerScore += 1;
+        playerscore.innerText = playerScore; // Update player's score
+        return 'Player wins! Computer chose: ' + computerInput;
+    } else {
+        computerScore += 1;
+        computerscore.innerText = computerScore; // Update computer's score
+        return 'Computer wins! Computer chose: ' + computerInput;
+    }
+};
+
 playButton.addEventListener('click',()=>{
     play()
 })
